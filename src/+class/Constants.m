2d6a9337550376deac44ae79ae38bff8aa72b344
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2026-07-10 10:24:45 -0300

    Release v2.00.6 with image layout fix
    Bump application and configuration versions. Adjust ROMES image display settings in PMEF_2026 report template from 95% width/height to 100% width/auto height for better rendering.

--- src/+class/Constants.m
+++ src/+class/Constants.m
@@ -3,7 +3,7 @@ classdef (Abstract) Constants
     properties (Constant)
         %-----------------------------------------------------------------%
         appName       = 'appAnalise'
-        appVersion    = '2.00.5'
+        appVersion    = '2.00.6'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 950, 660]
